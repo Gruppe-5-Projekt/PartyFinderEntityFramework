7eaaab74e4662c80653c70a86378caaf2b991f10
Vigtor <73114751+VigtorWolf@users.noreply.github.com>
2021-11-15 12:19:21 +0100

    WinForms + MVC solution

---
+++ PartyFinderGUI/PartyFinderMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PartyFinderMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
