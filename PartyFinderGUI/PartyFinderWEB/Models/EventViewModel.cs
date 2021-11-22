e0fa6e526cbdd8002f6a21041e24d9b55fbae925
VigtorWolf <73114751+VigtorWolf@users.noreply.github.com>
2021-11-22 13:32:11 +0100

    Dropdown menu for Event

---
+++ PartyFinderGUI/PartyFinderWEB/Models/EventViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace PartyFinderWEB.Models
+{
+    public class EventViewModel
+    {
+
+    public EventViewModel(string eventName, int eventCapacity, DateTime startDateTime, DateTime endDateTime, string description, int profileId)
+    {
+        EventName = eventName;
+        EventCapacity = eventCapacity;
+        StartDateTime = startDateTime;
+        EndDateTime = endDateTime;
+        Description = description;
+        ProfileId = profileId;
+    }
+
+    public int Id { get; set; }
+    public string EventName { get; set; } = null!;
+    public int EventCapacity { get; set; }
+    public DateTime StartDateTime { get; set; }
+    public DateTime EndDateTime { get; set; }
+    public string? Description { get; set; }
+    public int ProfileId { get; set; }
+}
+}
+
