5d3bd14857d84511eecc424a97e1eee6cf0cb778
VigtorWolf <73114751+VigtorWolf@users.noreply.github.com>
2021-11-26 13:17:08 +0100

    PROFILE. Mangler ProfileAcces + API Profile funkt.
    Det er værd at påpege, at skulle der ske en error i koden, bliver der stadig skabt en user i MVC'en. Hvilket betyder at der eksisterer en User uden en Profile.

---
+++ PartyFinderGUI/PartyFinderWEB/Controllers/ProfileController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyFinderWEB.Models;
+using System.Security.Claims;
+
+namespace PartyFinderWEB.Controllers
+{
+    public class ProfileController : Controller
+    {
+        /*
+        ProfileServiceAccess _pAccess;
+
+        public ProfileController()
+        {
+            _pAccess = new ProfileServiceAccess();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<int> SaveProfile(string firstName, string lastName, DateTime age, string gender)
+        {
+            int insertedId = -1;
+            //HOW, database user != database profile. Hvordan hækler vi dem sammen? Identity er en string, men bliver converted til en int?
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+            var aspNetFK = claim.Value;
+            if (aspNetFK != null)
+            {
+                ProfileViewModel newProfile = new ProfileViewModel(firstName, lastName, age, gender, aspNetFK);
+                insertedId = await _pAccess.SaveEvent(newProfile);
+            }
+            else
+            {
+
+            }
+            return insertedId;
+
+        }
+        */
+    }
+}
---
+++ PartyFinderGUI/PartyFinderWEB/Models/ProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PartyFinderWEB.Models
+{
+    public class ProfileViewModel
+    {
+        public ProfileViewModel(string firstName, string lastName, DateTime age, string gender, string aspNetFK)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Gender = gender;
+            AspNetFK = aspNetFK;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Age { get; set; }
+        public string Gender { get; set; }
+        public string AspNetFK { get; set; }
+
+    }
+}
