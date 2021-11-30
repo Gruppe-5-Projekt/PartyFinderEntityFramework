271c8004d23ffa564bf184ed478c106d21c83139
Vigtor <73114751+VigtorWolf@users.noreply.github.com>
2021-11-30 14:29:47 +0100

    Update MatchController.cs

--- PartyFinderGUI/PartyFinderWEB/Controllers/MatchController.cs
+++ PartyFinderGUI/PartyFinderWEB/Controllers/MatchController.cs
@@ -9,16 +9,45 @@ namespace PartyFinderWEB.Controllers
             return View();
         }
 
-        //public void match(MatchIds)
-        //{
-        //    Db.add(matchids);
-        //    CheckCap(); //bReAkPoInT!
-        //    CheckIsSpace(); //ElLeR wHaTeVeR dE mEtOdEr HeDdEr
+        public async Task<ActionResult> Like(string eventName, int eventCapacity, DateTime startDateTime, DateTime endDateTime, string description)
+        {
+            int insertedId = -1;
+            //HOW, database user != database profile. Hvordan hækler vi dem sammen? Identity er en string, men bliver converted til en int?
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+            var aspNetFK = claim.Value;
 
-        //    if(space<capacity)
-        //    {
-        //        dB.saveChanges();
-        //    }
-        //}
+            int profileId = -1;
+            if (aspNetFK != null)
+            {
+                EventViewModel newEvent = new EventViewModel(eventName, eventCapacity, startDateTime, endDateTime, description, aspNetFK, profileId);
+                insertedId = await _eAccess.SaveEvent(newEvent);
+            }
+            else
+            {
+
+            }
+            return View("CreatedEvent", insertedId as object);
+        }
+        public async Task<ActionResult> Dislike(string eventName, int eventCapacity, DateTime startDateTime, DateTime endDateTime, string description)
+        {
+            int insertedId = -1;
+            //HOW, database user != database profile. Hvordan hækler vi dem sammen? Identity er en string, men bliver converted til en int?
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+            var aspNetFK = claim.Value;
+
+            int profileId = -1;
+            if (aspNetFK != null)
+            {
+                EventViewModel newEvent = new EventViewModel(eventName, eventCapacity, startDateTime, endDateTime, description, aspNetFK, profileId);
+                insertedId = await _eAccess.SaveEvent(newEvent);
+            }
+            else
+            {
+
+            }
+            return View("CreatedEvent", insertedId as object);
+        }
     }
 }
