bc71e019b5e91d06d4ea7f2892b3f76349257180
Vigtor <1086219@UCN.dk>
2021-12-07 14:49:16 +0100

    Update MatchAccess.cs

--- PartyFinderAPI/PartyFinderData/DatabaseLayers/Match/MatchAccess.cs
+++ PartyFinderAPI/PartyFinderData/DatabaseLayers/Match/MatchAccess.cs
@@ -41,7 +41,6 @@ namespace PartyFinderData.DatabaseLayers
         public int CheckAndCommitMatchPublic(Match match)
         {
             int eventId = match.EventId;
-            int profileId = match.ProfileId;
             bool isMatched = match.Match1;
             int status = -1;
             try
@@ -73,35 +72,13 @@ namespace PartyFinderData.DatabaseLayers
             }
             catch
             {
-                status = -1;
             }
-
             return status;
         }
-
-        //Koden tjekker på databasen om der er plads, og comitter en profil.
-        //if (matchAmount < capacity)
-        //{
-        //    Console.WriteLine("Inserting a new match");
-        //    db.Matches
-        //        .Add(match);
-        //    Console.WriteLine("Breakpoint her.");
-        //    //Koden tjekker igen om der er plads og ruller tilbage hvis der er for mange.
-        //    int allMatchesNow = CheckCurrentMatches(eventId);
-        //    if (allMatchesNow < capacity)
-        //    {
-        //        db.SaveChanges();
-        //        status = 0;
-        //    }
-        //    else
-        //    {
-        //        status = -2;
-        //    }
 
         public Event GetRandomEvent(int profileId)
         {
             bool complete = false;
-            // Så længe while loopet ikke er true, fortsætter den.
             var foundEvents = db.Events
                         .Where(e => e.EndDateTime > DateTime.Now)
                         .Where(e => e.ProfileId != profileId)
@@ -114,9 +91,6 @@ namespace PartyFinderData.DatabaseLayers
                     foundEvents.Remove(itemToRemove);
                 }
                 int count = foundEvents.Count;
-
-
-                // Vælger et tilfældigt event, af de events funktionen over lige har fundet.
                 if (count > 0)
                 {
                     int r = rnd.Next(count);
@@ -127,43 +101,27 @@ namespace PartyFinderData.DatabaseLayers
                     foundEvent.Id = -1;
                     complete = true;
                 }
-
-                // Laver en liste over matches, og placerer dem i en ICollection på eventet.
                 foundEvent.Matches = db.Matches
                     .Where(m => m.EventId == foundEvent.Id)
                     .ToList();
 
                 int capacity = foundEvent.EventCapacity;
-                int matchAmount = foundEvent.Matches.Count;
-
-                // Tjekker om der er matches på eventet.
+                int matchAmount = CheckCurrentMatches(foundEvent.Id);
                 if (matchAmount > 0)
                 {
-                    // foreach der tjekker igennem ICollection af Matches på eventet.
                     foreach (Match item in foundEvent.Matches)
                     {
-                        // Tjekker at man ikke allerede er matched.
-                        if (item.ProfileId == profileId)
+                        if (item.ProfileId == profileId || matchAmount == capacity)
                         {
                             eventToRemove = item.EventId;
                             complete = false;
                         }
                         else
                         {
-                            // Tjekker om der er plads på eventet.
-                            if (matchAmount == capacity)
-                            {
-                                eventToRemove = item.EventId;
-                                complete = false;
-                            }
-                            else
-                            {
-                                complete = true;
-                            }
+                            complete = true;
                         }
                     }
                 }
-                // Hvis der ikke er matches, tilføjes man uanset, da vi antager at der altid er plads på et event med 0 matches.
                 else
                 {
                     complete = true;
