a0db9b3e0962ebf63372ba6fcec51677eaff4ae7
Vigtor <1086219@UCN.dk>
2021-12-09 16:13:36 +0100

    Update MatchServiceAccess.cs

--- PartyFinderGUI/PartyFinderWEB/ServiceLayer/MatchServiceAccess.cs
+++ PartyFinderGUI/PartyFinderWEB/ServiceLayer/MatchServiceAccess.cs
@@ -135,15 +135,15 @@ namespace PartyFinderWEB.ServiceLayer
                 response = await _httpClient.PostAsync(uri, content);
                 string resultingIdString = await response.Content.ReadAsStringAsync();
 
-                if (response.Content.Equals(200))
+                if (response.StatusCode.Equals(200))
                 {
                     Int32.TryParse(resultingIdString, out insertedMatchId);
                 }
-                    else if (response.Content.Equals(201))
+                    else if (response.StatusCode.Equals(201))
                 {
                     insertedMatchId = -1;
                 }
-                    else if (response.Content.Equals(403))
+                    else if (response.StatusCode.Equals(403))
                 {
                     insertedMatchId = -2;
                 }
